2fd7eaa7affb84779938bdaf675c84f11fd62e7f
leziga-bakor <leziga_bakor@epam.com>
2023-06-30 01:44:45 +0200

    implemented mapmaker_exceptions_start and test_exceptions_start

--- tests/chp2/video3/test_exceptions_start.py
+++ tests/chp2/video3/test_exceptions_start.py
@@ -1,3 +1,5 @@
+import pytest
+
 from scripts.chp2.video3.mapmaker_exceptions_start import Point
 
 
@@ -7,6 +9,8 @@ def test_make_one_point():
 
 
 def test_invalid_point_generation():  # TO DO
-    # with pytest.raises(Exception) as exp:
-    #     raise(Exception)
-    pass
+    with pytest.raises(Exception) as exp:
+        Point("Buenos Aires", 12.11386, -555.08269)
+    # breakpoint()
+
+    assert str(exp.value) == "Invalid latitude, longitude combination."
